8ed47c1650edb363595ab0c298e7c9e6b84e5781
Frank Mayer <mail@frank-mayer.io>
2022-12-25 06:39:22 +0100

    Added TypeScript types and fixed strange touch behaviour (#23)
    * only use mouse events when mouse controlled
    
    * added types

---
+++ dist/TagCloud.d.ts
@@ -0,0 +1,24 @@
+export interface TagCloud {
+    update(texts: Array<string>): void;
+    pause(): void;
+    resume(): void;
+    destroy(): void;
+}
+
+export interface TagCloudOptions {
+    radius?: number;
+    maxSpeed?: "slow" | "normal" | "fast";
+    initSpeed?: "slow" | "normal" | "fast";
+    deceleration?: number;
+    keep?: boolean;
+    containerClass?: string;
+    itemClass?: string;
+    useContainerInlineStyles?: boolean;
+    useItemInlineStyles?: boolean;
+}
+
+export default function (
+    container: string,
+    texts: Array<string>,
+    options: TagCloudOptions
+): TagCloud;
